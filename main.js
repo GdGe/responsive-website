ad2e1e4ee1a305b90be27d6573b9efe53a728404
GdGe <sone91961@gmail.com>
2021-11-03 16:34:12 +0900

    navbar 색상변경 js

---
+++ main.js
@@ -0,0 +1,8 @@
+'use sirict' ;
+
+//넷바 색상 변경
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+    if
+})
